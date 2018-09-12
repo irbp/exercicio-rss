e37e67fa4ac42879536dd282d075a29d5d393ccd
Ítalo Paulino <irbp@cin.ufpe.br>
2018-09-11 21:16:56 -0300

    modificação no layout e código comentado

--- RSS/app/src/main/java/br/ufpe/cin/if710/rss/RssListAdapter.kt
+++ RSS/app/src/main/java/br/ufpe/cin/if710/rss/RssListAdapter.kt
@@ -14,15 +14,18 @@ class RssListAdapter(private val rssList: List<ItemRSS>,
                      private val context: Context) : Adapter<RssListAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // inflando o layout de um item do recycler view
         val view = LayoutInflater.from(context)
                 .inflate(R.layout.itemlista, parent,false)
         return ViewHolder(view)
     }
 
+    // obtendo o número total de elementos da lista
     override fun getItemCount(): Int = rssList.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val itemRss = rssList[position]
+        // fazendo o bind da view com os valores
         holder.bind(itemRss)
     }
 
@@ -31,6 +34,7 @@ class RssListAdapter(private val rssList: List<ItemRSS>,
             val title = itemView.item_titulo
             val date = itemView.item_data
 
+            // preenchendo os campos de título e data da view
             title.text = itemRss.title
             date.text = itemRss.pubDate
 
